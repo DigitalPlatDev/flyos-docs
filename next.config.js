b64b0c3499f21c8f4aaba39704fb21353cee59d7
Edward Hsing <xingyujie50@gmail.com>
2024-02-08 18:27:25 +0800

    update

--- next.config.js
+++ next.config.js
@@ -4,20 +4,3 @@ const withNextra = require('nextra')({
 })
 
 module.exports = withNextra()
-// next.config.js
-/** @type {import('next').NextConfig} */
-
-const path = require('path');
-
-const nextConfig = {
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      '@': path.resolve(__dirname, './'),
-    };
-
-    return config;
-  },
-};
-
-module.exports = nextConfig;
