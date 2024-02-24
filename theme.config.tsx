7735dbd22dbc9d0df708c14e397de01bc3b285d8
Edward Hsing <xingyujie50@gmail.com>
2024-02-24 15:51:58 +0800

    Update theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,27 +1,23 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from 'react';
+import { DocsThemeConfig } from 'nextra-theme-docs';
 
 const config: DocsThemeConfig & { description: string } = {
   useNextSeoProps() {
     return {
       titleTemplate: '%s - FlyOS Docs'
-    }
+    };
   },
   logo: <span>FlyOS Docs</span>,
   project: {
-    link: 'https://github.com/EdwardLab/flyos',
+    link: 'https://github.com/EdwardLab/flyos'
   },
   chat: {
-    link: 'https://digitalplat.org/about-us/',
+    link: 'https://digitalplat.org/about-us/'
   },
   docsRepositoryBase: 'https://github.com/DigitalPlatDev/flyos-docs',
   footer: {
-    text: 'The FlyOS Project, by DigitalPlat.org',
+    text: 'The FlyOS Project, by DigitalPlat.org'
   },
-}
-
-export default {
-  ...config,
   head: (
     <>
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -29,4 +25,6 @@ export default {
       <meta property="og:description" content="FlyOS Documentation" />
     </>
   )
-}
+};
+
+export default config;
